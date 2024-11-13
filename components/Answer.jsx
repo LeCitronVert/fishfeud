0e28c14d8a1d6211735ddd3c9c99d57d6857bccd
Citron <contact@youceflcv.fr>
2024-11-13 22:07:15 +0100

    Click to display tips

--- components/Answer.jsx
+++ components/Answer.jsx
@@ -1,8 +1,25 @@
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+import "@sweetalert2/theme-dark/dark.css"
+
 const Answer = ({answer}) => {
     const className = 'answer-item' + (answer.found ? ' answer-found' : '');
 
+    const clickHandler = () => {
+        if (answer.found) {
+            return;
+        }
+
+        
+        withReactContent(Swal).fire({
+            title: answer.tip.img,
+            html: `<strong>${answer.tip.text}</strong>`,
+        });
+    };
+
     return (
-        <div className={className}>
+        <div className={className} onClick={clickHandler}>
             <span>{answer.score}</span>
             <span>{answer.answer}</span>
         </div>
